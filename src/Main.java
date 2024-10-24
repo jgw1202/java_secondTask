a2bbcea7a635e9f2c569cd1568c894755fd35c6f
jgw1202 <jgw1202@naver.com>
2024-10-24 17:33:44 +0900

    fix : ArrayList 선언 수정

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        List<Integer> gameTryList = new ArrayList<>();
+        List<Integer> gameTryList = new ArrayList<Integer>();
         while (true) {
             System.out.println("환영합니다! 원하시는 번호를 입력해주세요");
             System.out.println("1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기");
